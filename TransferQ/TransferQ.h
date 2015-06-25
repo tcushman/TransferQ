bac3dbb6653ac8d9a1c68672cc7528883a0902ca
tcushman <ted@homespotter.com>
2015-06-25 15:32:49 -0500

    Initial Commit

---
+++ TransferQ/TransferQ.h
@@ -0,0 +1,13 @@
+//
+//  TransferQ.h
+//  TransferQ
+//
+//  Created by Ted Cushman on 6/25/15.
+//  Copyright (c) 2015 Home Spotter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TransferQ : NSObject
+
+@end
---
+++ TransferQ/TransferQ.m
@@ -0,0 +1,13 @@
+//
+//  TransferQ.m
+//  TransferQ
+//
+//  Created by Ted Cushman on 6/25/15.
+//  Copyright (c) 2015 Home Spotter. All rights reserved.
+//
+
+#import "TransferQ.h"
+
+@implementation TransferQ
+
+@end
